7ae5e0f3af0e2e4aaa5aaed6928089fcddd01590
curtisqt30 <143355730+curtisqt30@users.noreply.github.com>
2025-05-10 15:27:19 -0700

    Update firebase-init.js

--- static/firebase-init.js
+++ static/firebase-init.js
@@ -1,11 +1,11 @@
-import { initializeApp } from "https://cdn.jsdelivr.net/npm/firebase@11.7.1/app/dist/index.esm.js";
-import { getStorage, ref, uploadBytes, getDownloadURL } from "https://cdn.jsdelivr.net/npm/firebase@11.7.1/storage/dist/index.esm.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.7.1/firebase-app.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/11.7.1/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDeb-bHUIm1vchbyuG_6S4MWYBlZYmMwIA",
   authDomain: "curtisconnect-a1630.firebaseapp.com",
   projectId: "curtisconnect-a1630",
-  storageBucket: "curtisconnect-a1630.firebasestorage.app",
+  storageBucket: "curtisconnect-a1630.appspot.com",
   messagingSenderId: "699870738851",
   appId: "1:699870738851:web:5ee9945895f1d723540216",
   measurementId: "G-CYHF4J5B49"
